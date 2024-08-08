f648fd946ea9a961487d34dd2508dfe3e59d58f1
Mark <markturnerv@gmail.com>
2024-08-08 16:14:32 -0600

    Now sending RS41 data over LoRa

--- RFM_Cient_withGPS_withTeensy/RFM_Client_withGPS_withTeensy/RFM_Client_withGPS_withTeensy.ino
+++ RFM_Cient_withGPS_withTeensy/RFM_Client_withGPS_withTeensy/RFM_Client_withGPS_withTeensy.ino
@@ -253,24 +253,8 @@ void loop()
 
   sensor_data = rs41.decoded_sensor_data(false);
   if (sensor_data.valid) {
-    Serial.print(sensor_data.frame_count); Serial.print(",");
-    Serial.print(sensor_data.air_temp_degC); Serial.print(",");
-    Serial.print(sensor_data.humdity_percent); Serial.print(",");
-    Serial.print(sensor_data.hsensor_temp_degC); Serial.print(",");
-    Serial.print(sensor_data.pres_mb); Serial.print(",");
-    Serial.print(sensor_data.internal_temp_degC); Serial.print(",");
-    Serial.print(sensor_data.module_status); Serial.print(",");
-    Serial.print(sensor_data.module_error); Serial.print(",");
-    Serial.print(sensor_data.pcb_supply_V); Serial.print(",");
-    Serial.print(sensor_data.lsm303_temp_degC); Serial.print(",");
-    Serial.print(sensor_data.pcb_heater_on); Serial.print(",");
-    Serial.print(sensor_data.mag_hdgXY_deg); Serial.print(",");
-    Serial.print(sensor_data.mag_hdgXZ_deg); Serial.print(",");
-    Serial.print(sensor_data.mag_hdgYZ_deg); Serial.print(",");
-    Serial.print(sensor_data.accelX_mG); Serial.print(",");
-    Serial.print(sensor_data.accelY_mG); Serial.print(",");
-    Serial.print(sensor_data.accelZ_mG);
-    Serial.println();
+    updateMode();
+    sendRS41();
   } else {
     Serial.println("Unable to obtain RS41 sensor data");
   }
